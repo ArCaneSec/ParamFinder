192d337f86f0b88cb76d974aabe2f2e88ada41b0
ArCaneSec <arcanebughunter@gmail.com>
2024-07-11 16:37:05 +0330

    updated regex patterns, more accurate patterns added, removed junk patterns

--- patterns.go
+++ patterns.go
@@ -3,14 +3,13 @@ package main
 import "regexp"
 
 var (
-	JsObjKeysPattern   = regexp.MustCompile(`(?:[{,]\s*(?:['"])?)([\w_-]+?)(?:\s*)(?:['"]?:)`) // JS object keys
-	htmlNamePattern    = regexp.MustCompile(`(?:<input.*?name)(?:="|')(.*?)(?:'|")`)           // html name keys
-	htmlIdPattern      = regexp.MustCompile(`(?:<input.*?id)(?:="|')(.*?)(?:'|")`)             // html id keys
-	JsVariablePattern  = regexp.MustCompile(`(?:(?:let|const|var)\s*)(\w+)`)                   // JS variable names
-	HttpPostParams     = regexp.MustCompile(`(?:&|^)([a-zA-Z+\d%]*?)=`)                        // http post parameters
-	HttpUrlParams      = regexp.MustCompile(`[?&](.*?)(?:=)`)                                  // http url parameters
-	HttpMultipartNames = regexp.MustCompile(` name="(.*?)"`)                                   // multipart data name keys
-	jsFiles            = regexp.MustCompile("(?:[\"'`])(.+\\.js(on)?)(?:[\"'`])")
+	JsObjKeysPattern  = regexp.MustCompile(`(?:[{,]\s*(?:['"])?)([\w]+?)(?:\s*)(?:['"]?:)`) // JS object keys
+	htmlNamePattern   = regexp.MustCompile(`(?:<input.*?name=)(?:"|')?(.*?)(?:'|"|\s)`)         // html name keys
+	htmlLabelNames    = regexp.MustCompile(`(?:<label.*?for=["']?)(.*?)(?:[>'" ])`)         // multipart data name keys
+	JsVariablePattern = regexp.MustCompile(`(?:^|[{(\s])(?:let|const|var\s*)(\w+)`)         // JS variable names
+	htmlIdPattern     = regexp.MustCompile(`(?:<input.*?id=)(?:'|")?(.*?)(?:'|"|\s)`)           // html id keys
+	jsFiles           = regexp.MustCompile("(?:[\"'`])(.+\\.js(on)?)(?:[\"'`])")            // js files path in respones
+	HttpUrlParams     = regexp.MustCompile(`(?:[?&]|%3F|%26)([\w]+?)(?:=|%3D)`)             // http url parameters
 )
 
-var patterns = []*regexp.Regexp{htmlNamePattern, htmlIdPattern, JsVariablePattern, JsObjKeysPattern, HttpUrlParams, HttpPostParams, HttpMultipartNames}
+var patterns = []*regexp.Regexp{htmlNamePattern, htmlIdPattern, JsVariablePattern, JsObjKeysPattern, HttpUrlParams, htmlLabelNames}
